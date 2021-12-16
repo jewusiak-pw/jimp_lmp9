96e95f06d2a674366e17527fa76f3148aa53a29c
Grzegorz Jewusiak <grzegorz@jewusiak.pl>
2021-12-16 17:58:56 +0100

    Dodanie testów automatycznych test-os, test-{1,4}. Zmiana formatowania w plikach wynikowych.

--- Makefile
+++ Makefile
@@ -1,8 +1,78 @@
-all: 
+all: bin-dir
 	cc -Wall --pedantic src/*.c -o bin/gauss
 
-debug:
+
+debug: bin-dir
 	cc -Wall --pedantic -ggdb src/*.c -o bin/gauss
 
-test: all
-	bin/gauss dane/A dane/b  
+bin-dir:
+	@mkdir -p bin
+
+test-dir:
+	@mkdir -p bin/tests
+
+test-os: test-dir all
+	@bin/gauss dane/A-os dane/B-os > bin/tests/output-os ;\
+	excd=$$? ; \
+	if [ $$excd -eq 3 ]; \
+	then \
+	  echo "Test macierzy osobliwej: OK (exit code: $$excd)";\
+	else \
+	  echo "Test macierzy osobliwej: BLAD! (exit code: $$excd)";\
+	fi
+
+
+test-1: test-dir all
+	@bin/gauss dane/d1A dane/d1b > bin/tests/output-t1; \
+	excd=$$? ; \
+    res=$$(diff -b bin/tests/output-t1 dane/w1) ; \
+    if [ -z "$$res" ]; \
+    then \
+      echo "Test 1: OK (exit code: $$excd)"; \
+    else \
+      echo "Test 1: BLAD! (exit code: $$excd)  Szczegoly:"; \
+      echo $$res ; \
+    fi
+
+
+test-2: test-dir all
+	@bin/gauss dane/d2A dane/d2b > bin/tests/output-t2; \
+	excd=$$? ; \
+    res=$$(diff -b bin/tests/output-t2 dane/w2) ; \
+    if [ -z "$$res" ]; \
+    then \
+      echo "Test 2: OK (exit code: $$excd)"; \
+    else \
+      echo "Test 2: BLAD! (exit code: $$excd)  Szczegoly:"; \
+      echo $$res ; \
+    fi
+
+
+test-3: test-dir all
+	@bin/gauss dane/d3A dane/d3b > bin/tests/output-t3; \
+	excd=$$? ; \
+    res=$$(diff -b bin/tests/output-t3 dane/w3) ; \
+    if [ -z "$$res" ]; \
+    then \
+      echo "Test 3: OK (exit code: $$excd)"; \
+    else \
+      echo "Test 3: BLAD! (exit code: $$excd)  Szczegoly:"; \
+      echo $$res ; \
+    fi
+
+
+
+test-4: test-dir all
+	@bin/gauss dane/d4A dane/d4b > bin/tests/output-t4; \
+	excd=$$? ; \
+    if [ $$excd -eq 4 ]; \
+    then \
+      echo "Test 4: OK (exit code: $$excd)"; \
+    else \
+      echo "Test 4: BLAD! (exit code: $$excd)"; \
+      echo $$res ; \
+    fi
+
+
+
+test: test-os test-1 test-2 test-3 test-4
--- src/main.c
+++ src/main.c
@@ -1,51 +1,60 @@
 #include "gauss.h"
 #include "backsubst.h"
 #include "mat_io.h"
-#include <stdlib.h>
 #include <stdio.h>
 
 /*
  * Zwraca:
- * -1 - zły format macierzy A
- * -2 - zły format macierzy b
- * -3 - dzielenie przez 0
- * -4 - złe wymiary macierzy
+ *  0 - poprawne działanie
+ *  1 - problem ładowania macierzy A
+ *  2 - problem ładowania macierzy b
+ *  3 - dzielenie przez 0
+ *  4 - złe wymiary macierzy
+ *  5 - za mało argumentów
  */
-int main(int argc, char ** argv) {
-	int res;
-	Matrix * A = readFromFile(argv[1]);
-	Matrix * b = readFromFile(argv[2]);
-	Matrix * x;
+int main(int argc, char **argv) {
+    int res;
+    if(argc<3){
+        puts("Za mala liczba argumentow!");
+        return 5;
+    }
 
-	if (A == NULL) return -1;
-	if (b == NULL) return -2;
-	printToScreen(A);
-	printToScreen(b);
+    Matrix *A = readFromFile(argv[1]);
+    Matrix *b = readFromFile(argv[2]);
+    Matrix *x;
 
-	res = eliminate(A,b);
+    if (A == NULL) return 1;
+    if (b == NULL) return 2;
 
-	x = createMatrix(b->r, 1);
-	if (x != NULL) {
-        if (res==1)
+
+    if ((res = eliminate(A, b)) == 1) {
+        puts("Dzielenie przez 0!");
+        return 3;
+    }
+
+    //printToScreen(A);
+    //printToScreen(b);
+
+    x = createMatrix(b->r, 1);
+    if (x != NULL) {
+
+        res = backsubst(x, A, b);
+        if (res == 2) {
+            puts("Złe wymiary macierzy!");
+            return 4;
+        }
+        if (res == 1) {
             puts("Dzielenie przez 0!");
+            return 3;
+        }
+        printToScreen(x);
+        freeMatrix(x);
+    } else {
+        fprintf(stderr, "Błąd! Nie mogłem utworzyć wektora wynikowego x.\n");
+    }
 
-		res = backsubst(x,A,b);
-		if (res==2)
-		{
-			puts("Złe wymiary macierzy!");
-		}
-		if (res==1)
-		{
-			puts("Dzielenie przez 0!"); 
-		}
-		printToScreen(x);
-	  freeMatrix(x);
-	} else {
-					fprintf(stderr,"Błąd! Nie mogłem utworzyć wektora wynikowego x.\n");
-	}
+    freeMatrix(A);
+    freeMatrix(b);
 
-	freeMatrix(A);
-	freeMatrix(b);
-
-	return 0;
+    return 0;
 }
